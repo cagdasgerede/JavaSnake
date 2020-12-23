664c2b194d89aed09451aa20b697ec91e8498edf
beyzaydin <beyzaaydin@etu.edu.com>
2020-12-24 01:27:47 +0300

    some indentations fixed

--- src/test/java/pl/nogacz/snake/application/MenuTest.java
+++ src/test/java/pl/nogacz/snake/application/MenuTest.java
@@ -6,18 +6,14 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import pl.nogacz.snake.application.Design;
 import pl.nogacz.snake.board.Board;
 import pl.nogacz.snake.board.Coordinates;
 
-import javax.swing.JFrame;
-
 public class MenuTest {
     Design design;
     Board board;
-    JFrame frame;
     
     @BeforeEach
     public void setup(){
@@ -38,16 +34,17 @@ public class MenuTest {
 
     @Test 
     void testMenuCoordinate(){
-        assertTrue(board.isMenuCoordinate(new Coordinates(1,1)));
+        assertTrue(board.isMenuCoordinate(new Coordinates(1, 1)));
     }
 
     @Test 
     void testNotMenuCoordinate(){
-        assertFalse(board.isMenuCoordinate(new Coordinates(3,4)));
+        assertFalse(board.isMenuCoordinate(new Coordinates(3, 4)));
     }
 
     @AfterEach
     public void tearDown() {
-        frame = null;
-  }
+        board = null;
+        design = null;
+    }
 }
